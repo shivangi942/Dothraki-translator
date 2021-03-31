b56c9d72954f4089e269c7688167159d879b907f
Shivangi Dubey <shivangi942@users.noreply.github.com>
2021-04-01 03:37:15 +0530

    Added translation API

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 var btntranslate = document.querySelector("#btn-translate");
-btntranslate.addEventListener("click", clickEventHandler);
-function clickEventHandler(){
-    console.log("clicked!!!!!!!!!!!!")
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function getTranslationURL(text){
+
+    return serverURL + "?text=" + text;
 }
 
+function errorHandler(error){
+    console.log("error ocurred: ", error);
+    alert("There is something wrong with the server! please try later.")
+}
+
+  function clickEventHandler(){
+      var inputtext = txtInput.value;
+      
+      fetch(getTranslationURL(inputtext))
+      .then(response => response.json())
+      .then(json => {
+          txtOutput.innerText = json.contents.translated;
+      } )
+      .catch(errorHandler);
+ }
+
+btntranslate.addEventListener("click", clickEventHandler);
